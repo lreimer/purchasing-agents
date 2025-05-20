4276e1cb25fd24ca23f6132aadb2c9b2ade25180
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2025-05-21 01:30:50 +0200

    Added initial skeleton for CRM tools. #2

--- crm-erp-mcp-tools/crm/tools.go
+++ crm-erp-mcp-tools/crm/tools.go
@@ -1,7 +1,33 @@
 package crm
 
-import "github.com/mark3labs/mcp-go/server"
+import (
+	"context"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
+)
 
 // add all CRM system tools
 func AddCrmTools(s *server.MCPServer) {
+	searchCustomer(s)
+}
+
+func searchCustomer(s *server.MCPServer) {
+	tool := mcp.NewTool("search_customer",
+		mcp.WithDescription("Sucht nach einem Kunden im CRM System"),
+		mcp.WithOpenWorldHintAnnotation(true),
+		mcp.WithString("kundenNummer",
+			mcp.Description("Die Kundennummer des Kunden im CRM System"),
+			mcp.Required(),
+		),
+	)
+
+	s.AddTool(tool, func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		kundenNummer, err := request.RequireString("kundenNummer")
+		if err != nil {
+			return mcp.NewToolResultError("kundenNummer is required"), nil
+		}
+
+		return mcp.NewToolResultText(kundenNummer), nil
+	})
 }
